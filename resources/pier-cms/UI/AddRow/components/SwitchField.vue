6e88db78b48c2cc9b217bb21d8da97fa676583cd
Walter <wakyj07@gmail.com>
2021-10-25 14:31:46 +0300

    Fix bug in boolean form field

--- resources/pier-cms/UI/AddRow/components/SwitchField.vue
+++ resources/pier-cms/UI/AddRow/components/SwitchField.vue
@@ -1,39 +1,64 @@
 <template>
-    <label style="display: flex" class="capitalize items-center">
-        <vs-switch val="false" v-model="val" />
+    <label style="display: flex" class="cursor-pointer justify-start items-center capitalize"
+        @click="val = val === 0 ? 1 : 0"
+    >
+        <span class="rounded-full overflow-hidden relative flex items-center"
+            :class="{
+                'justify-end': val === 1,
+            }"
+            style="width: 42px; padding: 1px; padding-bottom: 1px; border: 1px solid #ddd"
+            :style="{background: val === 1 ? '#2c5282' : '#eee'}"
+        >
+            <span class="block rounded-full bg-white"
+                style="width: 18px; height: 18px; border: 1px solid #ddd"
+            ></span>
+        </span>
 
-        <span class="ml-4" style="margin-bottom: 0.3rem;">
+        <span class="ml-3">
             {{ label }}
         </span>
     </label>
 </template>
 
 <script>
-import { vsSwitch } from 'vuesax';
-import 'vuesax/dist/vuesax.css';
-
 export default {
     name: "SwitchField",
     props: {
         label: String,
-        value: Boolean
+        value: Number|String
     },
     mounted() {
-        if(this.value)
+        if(this.value === 0 || this.value === 1)
             this.val = this.value;
+        else{
+            this.val = 0;
+            this.$emit('input', 0);
+        }
     },
     data() {
         return {
-            val: false
+            val: 0,
+            dontUpdate: false
         }
     },
     watch: {
+        value: function(newValue){
+            this.dontUpdate = true;
+            if(newValue === 0 || newValue === 1)
+                this.val = this.value;
+            else{
+                this.val = 0;
+                this.$emit('input', 0);
+            }
+        },
         val: function(newValue){
+            // if(this.dontUpdate){
+            //     this.dontUpdate = false;
+            //     return;
+            // }
+
             this.$emit('input', newValue);
         }
     },
-    components: {
-        vsSwitch
-    }
 }
 </script>
